332a56d20497253f348b7f0b8fd3695343f819b7
Sandra <Sandra@DEP-INF1>
2022-11-02 17:45:30 +0100

    prueba potencia

---
+++ src/learningGitHub/prueba.java
@@ -0,0 +1,19 @@
+package learningGitHub;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		potenciaDos(3);
+	}
+
+	static Integer potenciaDos(Integer num) {
+		Integer result;
+		if (num!=2) {
+		result = (int) Math.pow(num, 2);
+		}else {
+			result = 3;
+		}
+		return result;
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Sandra
+ *
+ */
+module learningGitHub {
+}
